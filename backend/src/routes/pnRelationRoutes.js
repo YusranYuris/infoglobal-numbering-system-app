1437687d492f5c63ebef1fb752c87cfe918d35ad
YusranYuris <yusranyuris06@gmail.com>
2026-06-16 14:27:51 +0700

    DELETE added to pnRelationController

--- backend/src/routes/pnRelationRoutes.js
+++ backend/src/routes/pnRelationRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { 
     createPnRelation, 
+    deletePnRelation, 
     getPnStructure
 } from "../controllers/pnRelationController.js"
 import { authenticate } from "../middleware/authMiddleware.js";
@@ -14,5 +15,8 @@ router.post("/", authenticate, createPnRelation)
 
 // Get PN Relation Structure
 router.get("/:rootId", authenticate, getPnStructure)
+
+// Delete PN Relation
+router.delete("/:id", authenticate, deletePnRelation)
 
 export default router;
